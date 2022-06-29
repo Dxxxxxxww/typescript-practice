425a4afe03d3a81c1e916b144cf7b3107121d34c
Dxxxxxxww <gotu2018@163.com>
2022-06-29 17:26:39 +0800

    add array-methods

--- src/array-methods.ts
+++ src/array-methods.ts
@@ -1,10 +1,25 @@
 // first
 type MyFirst<T extends any[]> = T extends [infer F, ...infer Rest] ? F : never;
 type mf1 = MyFirst<[1, 2, 3]>;
+
 // last
 type MyLast<T extends any[]> = T extends [...infer Rest, infer L] ? L : never;
 type ml1 = MyLast<[1, 2, 3]>;
+
 // concat
 type MyConcat<T extends any[], U extends any[] = []> = [...T, ...U];
 type mc1 = MyConcat<[1, 2, 3], [4, 5, 6]>;
 type mc2 = MyConcat<[1, 2, 3]>;
+
+// push
+type MyPush<T extends any[], U> = [...T, U];
+type mp1 = MyPush<[1, 2, 3], 4>;
+
+// unshift
+type MyUnshift<T extends any[], U> = [U, ...T];
+type mus = MyUnshift<[1, 2, 3], 4>;
+
+// includes 通过 number 来表示下标，获取 T 的所有 value 作为字面量联合类型
+type MyIncludes<T extends any[], U> = U extends T[number] ? true : false;
+type mi1 = MyIncludes<[1, 2, 3], 4>;
+type mi2 = MyIncludes<[1, 2, 3], 2>;
