30402c93fede0b1898ffb11c5b07150da3a4b0f5
Dxxxxxxww <gotu2018@163.com>
2022-07-06 09:36:58 +0800

    includes，flat，pop 。extend

--- src/basic/array-methods.ts
+++ src/basic/array-methods.ts
@@ -15,9 +15,19 @@ type mc2 = MyConcat<[1, 2, 3]>;
 type MyPush<T extends any[], U> = [...T, U];
 type mp1 = MyPush<[1, 2, 3], 4>;
 
-// pop
+// pop 这里返回的是剩余的数组类型，而不是跟 js 一样，返回被删除的值。
+// 因为 ts 类型不能操作内存，拿不到剩余数组，而 js 可以通过操作内存来拿到剩余数组，所以 js 返回的是值，js 和 ts 还是要做区分的
 type MyPop<T extends any[]> = T extends [...infer L, infer R] ? L : never;
 type mpo1 = MyPop<[1, 2, 3]>;
+
+// pop 扩展1，解决空数组问题
+type MyPop2<T extends any[]> = T extends []
+  ? []
+  : T extends [...infer L, infer R]
+  ? L
+  : never;
+type mpop2 = MyPop2<[]>;
+type mpop3 = MyPop2<[1, 2, 3]>;
 
 // unshift
 type MyUnshift<T extends any[], U> = [U, ...T];
@@ -31,3 +41,27 @@ type ms1 = MyShift<[1, 2, 3]>;
 type MyIncludes<T extends any[], U> = U extends T[number] ? true : false;
 type mi1 = MyIncludes<[1, 2, 3], 4>;
 type mi2 = MyIncludes<[1, 2, 3], 2>;
+
+// includes 扩展，支持对象对比
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
+type MyIncludes2<T extends any[], U> = T extends [infer F, ...infer Rest]
+  ? Equal<F, U> extends true
+    ? true
+    : MyIncludes2<Rest, U>
+  : false;
+
+type mi3 = MyIncludes2<[{ id: 1 }], { id: 1 }>;
+type mi4 = MyIncludes2<[{ id: 1 }], { id: 2 }>;
+type mi5 = MyIncludes2<[{ id: 1 }], {}>;
+
+// Flatten
+type MyFlat<T extends any[]> = T extends [infer L, ...infer Rest]
+  ? L extends any[]
+    ? [...MyFlat<L>, ...MyFlat<Rest>]
+    : [L, ...MyFlat<Rest>]
+  : [];
+type mf1 = MyFlat<[1, [2, [3, [4]]]]>;
