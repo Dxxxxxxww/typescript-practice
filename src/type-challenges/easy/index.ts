178019c6f63e3111cef945cfdb818bf64af347df
Dxxxxxxww <gotu2018@163.com>
2022-07-20 17:25:17 +0800

    feat: tc,easy,3

--- src/type-challenges/easy/index.ts
+++ src/type-challenges/easy/index.ts
@@ -22,3 +22,29 @@ type TCTupleToObject<T extends any[]> = {
   [P in T[number]]: P;
 };
 type tctto1 = TCTupleToObject<typeof tuple>;
+
+// FirstOfArray
+type TCFirstOfArray<T extends any[]> = T extends [infer L, ...R] ? L : never;
+
+type arr1 = ['a', 'b', 'c'];
+type arr2 = [3, 2, 1];
+
+type tcfoa1 = TCFirstOfArray<arr1>; // expected to be 'a'
+type tcfoa2 = TCFirstOfArray<arr2>; // expected to be 3
+
+// LastOfArray
+type TCLastOfArray<T extends any[]> = T extends [...L, infer R] ? R : never;
+
+type tcloa1 = TCLastOfArray<arr1>; // expected to be 'a'
+type tcloa2 = TCLastOfArray<arr2>; // expected to be 3
+
+// Length of Tuple
+type TCLengthOfTuple<T extends readonly any[]> = T['length'];
+type spaceX = [
+  'FALCON 9',
+  'FALCON HEAVY',
+  'DRAGON',
+  'STARSHIP',
+  'HUMAN SPACEFLIGHT'
+];
+type tcot1 = TCLengthOfTuple<spaceX>;
